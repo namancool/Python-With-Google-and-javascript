43cab7b38d90e5bf9d5f63daaac2a63a4c688e81
Naman <namansharmanaman1111@gmail.com>
2021-08-06 20:16:31 +0530

    5th commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// 1.Ways to print in javascript
+    //console.log("Hello World");
+    //alert("good job !!!");
+    //document.write("this is a document write...")
+    
+    // 2.Javascript console API
+    console.log("Hello World", 100+1, "JavaScript learning");
+    console.warn("this is warning");
+    console.error("This is an error!!")
