4461ffe48643c83abf90d71d335eebdce55ba401
Naman <namansharmanaman1111@gmail.com>
2021-08-07 18:17:51 +0530

    9th commit

--- index.js
+++ index.js
@@ -4,6 +4,12 @@
 //document.write("this is a document write...")
 
 // 2.Javascript console API
-console.log("Hello World", 100 + 1, "JavaScript learning");
-console.warn("this is warning");
-console.error("This is an error!!")
+// console.log("Hello World", 100 + 1, "JavaScript learning");
+// console.warn("this is warning");
+// console.error("This is an error!!")
+
+// 3. Javascript Variables
+// What are variables? - Container to store data values 
+var number1 = 54;
+var number2 = 67;
+console.log(number1 + number2);
