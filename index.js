bd092006bf919033df2ecfab9f21c16113cd7570
Naman <namansharmanaman1111@gmail.com>
2021-08-10 01:49:41 +0530

    22nd commit

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ console.log("End")
 
 // Loops in javascript
 var arr = [1,2,3,4,5,6,7,8,9];
-console.log(arr);
+// console.log(arr);
 for(var i = 0; i<arr.length;i++)
 {
     console.log(arr[i]);
