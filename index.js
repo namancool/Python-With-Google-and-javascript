ed9843263aa05f6362c2ada7158b4f5f2bd51a07
Naman <namansharmanaman1111@gmail.com>
2021-08-11 19:35:34 +0530

    28th commit

--- index.js
+++ index.js
@@ -154,10 +154,14 @@ var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // const ac = 0;
 // ac++;
 let j = 0;
-while(j<arr.length)
+// while(j<arr.length)
+// {
+//     console.log(arr[j]);
+//     j++;
+// }
+do
 {
     console.log(arr[j]);
     j++;
-}
-
+}while(j< arr.length)
 
