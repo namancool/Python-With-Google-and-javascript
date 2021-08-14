3b4de11cf04595b3ba918dfafe57c625ad7e35d2
Naman <namansharmanaman1111@gmail.com>
2021-08-14 22:36:40 +0530

    45th Commit

--- index.js
+++ index.js
@@ -211,13 +211,24 @@ console.log(ElemClass);
 // ElemClass[1].style.background = "green";
 ElemClass[0].classList.add("bg-primary");
 ElemClass[0].classList.add("text-success");
-console.log(Elem.innerHTML);
-console.log(Elem.innerText);
-console.log(ElemClass[0].innerHTML);
-console.log(ElemClass[0].innerText);
+// console.log(Elem.innerHTML);
+// console.log(Elem.innerText);
+// console.log(ElemClass[0].innerHTML);
+// console.log(ElemClass[0].innerText);
+
+tn = document.getElementsByTagName('button');
+tn = document.getElementsByTagName('div');
+console.log(tn);
+
+createdElement = document.createElement('p');
+createdElement.innerText = "This is a new paragraph"
+tn[0].appendChild(createdElement);
+// tn[0].removeChild(createdElement);
 
 
-
-
+createdElement2 = document.createElement('b');
+createdElement2.innerText = "This is a new paragraph in bold";
+tn[0].replaceChild(createdElement2, createdElement);
+// removeChild(element); ---> removes an element
 
 
