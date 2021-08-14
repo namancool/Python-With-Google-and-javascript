293cc7a4aae070fd27897c62d12d29a371127c2c
Naman <namansharmanaman1111@gmail.com>
2021-08-15 00:14:28 +0530

    46th commit

--- index.js
+++ index.js
@@ -244,5 +244,24 @@ window.onload = function(){
     console.log('The document was loaded');
 }
 
+// Event in javascript
+firstContainer.addEventListener('click',function(){
+    // document.querySelectorAll('.container')[1].innerHTML = "<b> We have clicked</b>"
+    console.log('Click on container');
 
-
+})
+firstContainer.addEventListener('mouseover',function(){
+    console.log('Mouse on container');
+})
+firstContainer.addEventListener('mouseout',function(){
+    console.log('Mouse out of container');
+})
+let prevHTML = document.querySelectorAll('.container')[1].innerHTML;
+firstContainer.addEventListener('mouseup',function(){
+    document.querySelectorAll('.container')[1].innerHTML = prevHTML;
+    console.log('Mouse up when clicked on container');
+})
+firstContainer.addEventListener('mousedown',function(){
+    document.querySelectorAll('.container')[1].innerHTML = "<b> We have clicked</b>"
+    console.log('Mouse down when clicked on container');
+})
