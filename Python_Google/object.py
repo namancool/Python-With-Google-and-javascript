bbd68bd52d479ac9499fde09fcf3feff9abc9983
Naman Sharma <68382698+namancool@users.noreply.github.com>
2022-01-10 20:54:30 +0530

    commit bolte

---
+++ Python_Google/object.py
@@ -0,0 +1,35 @@
+#OOP
+class PlayerCharacter:
+    def __init__(self,name,roll_number,age):
+        self.name = name
+        self.roll_number = roll_number
+        self.age = age
+
+
+    def run(self):
+        print('run')
+
+player1 = PlayerCharacter('NAMAN SHARMA','20SCSE1180020',19)
+player2 = PlayerCharacter('ANKIT GUPTA','20SCSE1181003',21)
+
+print(player1.name)
+print(player1.roll_number)
+print(player1.age)
+print(player2.name)
+print(player2.roll_number)
+print(player2.age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
